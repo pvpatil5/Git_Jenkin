c5a394a85889c320a1afe8ec3219f67c6960f002
AMAR-G <AMAR-G@Amar>
2020-07-08 18:39:41 +0530

    Demo Commit

---
+++ src/pac/Sample1.java
@@ -0,0 +1,20 @@
+package pac;
+
+public class Sample1 {
+
+	public static void main(String[] args) 
+	{
+		String s1= "Selenium";
+		
+				char c[] = s1.toCharArray();
+		
+				for (int i = c.length-1; i >=0; i--) 
+				{
+					System.out.print(c[i]);
+				}
+		
+		
+
+	}
+
+}
